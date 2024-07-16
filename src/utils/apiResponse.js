d10decdcc11d5fef3a9fbf830b25f372e884f4c7
GhufranButt <ghufranbutt0322@example.com>
2024-07-16 22:01:01 +0500

    add user and video models

--- src/utils/apiResponse.js
+++ src/utils/apiResponse.js
@@ -8,12 +8,3 @@ class ApiResponse {
 }
 
 export { ApiResponse };
-
-class apiResponse {
-  constructor(statusCode, message = "Success", data) {
-    this.statusCode = statusCode;
-    this.data = data;
-    this.message = message;
-    this.success = statusCode < 4000;
-  }
-}
